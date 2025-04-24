f50abcac4a8b74b5f651f6fb1b4e48e80af37d59
Nammon <nammol05@gmail.com>
2025-04-24 20:32:20 +0700

    Update

--- frontend/frontend/app/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  return (
-    <header>
-      <h1 className=" text-white font-bold text-xl text-center m-3">
-        Musical Instuments
-      </h1>
-    </header>
-  );
-}
